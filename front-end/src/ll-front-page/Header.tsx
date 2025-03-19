f9e7b580690154069fc69204b8149b1c1572cefc
Shai-Shargal <shai.shargal@gmail.com>
2025-03-19 20:39:31 +0200

    made a new header

--- front-end/src/ll-front-page/Header.tsx
+++ front-end/src/ll-front-page/Header.tsx
@@ -1,15 +1,21 @@
-import './Header.css';
+import "./Header.css";
+import trophyIcon from "../assets/trophy-svgrepo-com (1).svg";
 
 function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <img src="/path/to/trophy-icon.svg" alt="Trophy Icon" className="logo-icon" />
+        <img src={trophyIcon} alt="Trophy Icon" className="logo-icon" />
         <h1>LeagueLink</h1>
       </div>
       <nav>
         <ul>
-          <li><a href="#">Who We Are</a></li>
+          <li>
+            <a href="#">Our Goal</a>
+          </li>
+          <li>
+            <a href="#">Meet the Team</a>
+          </li>
         </ul>
       </nav>
     </header>
@@ -17,4 +23,3 @@ function Header() {
 }
 
 export default Header;
-
