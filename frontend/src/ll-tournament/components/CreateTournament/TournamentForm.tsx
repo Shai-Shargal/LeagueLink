c9a66c955d1b2ece7061476707a699f657167efd
Shai-Shargal <shai.shargal@gmail.com>
2025-05-11 10:36:03 +0300

    Add dayjs dependency and integrate date/time pickers in TournamentForm

--- frontend/src/ll-tournament/components/CreateTournament/TournamentForm.tsx
+++ frontend/src/ll-tournament/components/CreateTournament/TournamentForm.tsx
@@ -1,4 +1,11 @@
 import { Box, TextField } from "@mui/material";
+import {
+  DatePicker,
+  TimePicker,
+  LocalizationProvider,
+} from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
 
 interface TournamentFormProps {
   newTournament: {
@@ -48,57 +55,74 @@ export const TournamentForm: React.FC<TournamentFormProps> = ({
       "& .MuiSvgIcon-root": {
         color: "white",
       },
+      "& .MuiInputAdornment-root .MuiSvgIcon-root": {
+        color: "white",
+      },
     },
   };
 
   return (
-    <Box sx={{ display: "flex", gap: 2 }}>
-      <TextField
-        label="Tournament Name"
-        value={newTournament.name}
-        onChange={handleChange("name")}
-        error={!!errors.name}
-        helperText={errors.name}
-        {...commonTextFieldProps}
-      />
-      <TextField
-        fullWidth
-        label="Location"
-        value={newTournament.location || ""}
-        onChange={(e) => onTournamentChange("location", e.target.value)}
-        error={!!errors.location}
-        helperText={errors.location}
-        disabled={isCreating}
-      />
-      <TextField
-        label="Date"
-        type="date"
-        value={newTournament.date}
-        onChange={handleChange("date")}
-        error={!!errors.date}
-        helperText={errors.date}
-        InputLabelProps={{ shrink: true }}
-        {...commonTextFieldProps}
-      />
-      <TextField
-        label="Time"
-        type="time"
-        value={newTournament.time}
-        onChange={handleChange("time")}
-        error={!!errors.time}
-        helperText={errors.time}
-        InputLabelProps={{ shrink: true }}
-        {...commonTextFieldProps}
-      />
-      <TextField
-        fullWidth
-        label="Number of Rounds (Best of X)"
-        type="number"
-        value={newTournament.rounds || 5}
-        onChange={(e) => onTournamentChange("rounds", parseInt(e.target.value))}
-        inputProps={{ min: 1, max: 9 }}
-        disabled={isCreating}
-      />
-    </Box>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Box sx={{ display: "flex", gap: 2 }}>
+        <TextField
+          label="Tournament Name"
+          value={newTournament.name}
+          onChange={handleChange("name")}
+          error={!!errors.name}
+          helperText={errors.name}
+          {...commonTextFieldProps}
+        />
+        <TextField
+          label="Location"
+          value={newTournament.location || ""}
+          onChange={(e) => onTournamentChange("location", e.target.value)}
+          error={!!errors.location}
+          helperText={errors.location}
+          {...commonTextFieldProps}
+        />
+        <DatePicker
+          label="Date"
+          value={newTournament.date ? dayjs(newTournament.date) : null}
+          onChange={(value) =>
+            onTournamentChange("date", value ? value.format("YYYY-MM-DD") : "")
+          }
+          slotProps={{
+            textField: {
+              error: !!errors.date,
+              helperText: errors.date,
+              ...commonTextFieldProps,
+            },
+          }}
+        />
+        <TimePicker
+          label="Time"
+          value={newTournament.time ? dayjs(newTournament.time, "HH:mm") : null}
+          onChange={(value) =>
+            onTournamentChange("time", value ? value.format("HH:mm") : "")
+          }
+          slotProps={{
+            textField: {
+              error: !!errors.time,
+              helperText: errors.time,
+              ...commonTextFieldProps,
+            },
+          }}
+        />
+        <TextField
+          label="Number of Rounds (Best of X)"
+          type="number"
+          value={newTournament.rounds || 5}
+          onChange={(e) => {
+            let val = parseInt(e.target.value);
+            if (isNaN(val) || val < 1) val = 1;
+            if (val > 9) val = 9;
+            if (val % 2 === 0) val = val - 1; // Ensure odd
+            onTournamentChange("rounds", val);
+          }}
+          inputProps={{ min: 1, max: 9, step: 2 }}
+          {...commonTextFieldProps}
+        />
+      </Box>
+    </LocalizationProvider>
   );
 };
